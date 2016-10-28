f6218e5f11e840a99f0412d50b6fd2c53942388f
luguanxing <651571925@qq.com>
2016-10-28 22:11:01 +0800

    first Kernel-Driver

--- 01helloworld/first.c
+++ 01helloworld/first.c
@@ -1,11 +1,16 @@
+//后缀名为C则以_stdcall调用
 #include <ntddk.h>
+#define INITCODE code_seg("INIT") 
+#pragma INITCODE
 
-VOID DriverUnload(PDRIVER_OBJECT driver) {
-	DbgPrint("loading!\n");
+VOID DDK_Unload (IN PDRIVER_OBJECT pDriverObject)
+{
+	KdPrint(("驱动成功卸载!"));
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING reg_path) {
-	DbgPrint("hello world!\n");
-	driver->DriverUnload = DriverUnload;
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject,PUNICODE_STRING B)
+{
+	KdPrint(("驱动成功加载!"));
+	pDriverObject->DriverUnload = DDK_Unload;
 	return STATUS_SUCCESS;
 }
