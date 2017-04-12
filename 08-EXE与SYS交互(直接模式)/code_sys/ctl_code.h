021fa995274065dacd768433f7855dd11ff124c5
luguanxing <651571925@qq.com>
2017-04-13 00:22:52 +0800

    EXE与SYS交互(直接模式)

---
+++ 08-EXE与SYS交互(直接模式)/code_sys/ctl_code.h
@@ -0,0 +1,9 @@
+﻿#ifndef CTL_CODE
+	#pragma message("---------\nEXE模式 . Include winioctl.h\n--------- ")
+	#include <WinIoCtl.h>
+#else
+	#pragma message("---------\nSYS模式 . No Include winioctl.h\n---------  ")
+#endif
+
+#define add_code CTL_CODE(FILE_DEVICE_UNKNOWN, 	0x800, 	METHOD_IN_DIRECT,FILE_ANY_ACCESS)
+#define sub_code CTL_CODE(FILE_DEVICE_UNKNOWN, 	0x801, 	METHOD_IN_DIRECT,FILE_ANY_ACCESS)
